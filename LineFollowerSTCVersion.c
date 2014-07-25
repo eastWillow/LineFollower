28d8a49dee38ba27fc6e58caa1c6af9ae0d0915d
eastWillow <bobgash2@gmail.com>
2014-07-25 15:50:49 +0800

    Send The Color Data Back

---
+++ LineFollowerSTCVersion.c
@@ -0,0 +1,62 @@
+#include <STC12C5A60S2.H>
+#include <intrins.h>
+#define ADC_POWER 0x80
+#define ADC_FLAG 0x10
+#define ADC_START 0x08
+#define ADC_HHSPEED 0x60
+/*
+CNY70 > 3F =>BLACK
+*/
+unsigned char ch = 0;
+void setup();
+void serialInit(void);
+void serialSendHex(unsigned char);
+void Delay500ms();
+void main(){
+	setup();
+	serialInit();
+	while(1);
+}
+void setup(){
+	IE = 0xa0;
+	P1ASF = 0x1F;
+	ADC_CONTR = 0xe8;
+	_nop_();_nop_();_nop_();_nop_();_nop_();_nop_();_nop_();_nop_();_nop_();_nop_();_nop_();
+}
+void AnalogISR(void) interrupt 5 using 1{
+	ADC_CONTR &= ~ADC_FLAG;
+	serialSendHex(ADC_RES);
+	//Delay500ms();
+	ADC_CONTR = ADC_POWER|ADC_HHSPEED|ADC_START|ch;
+}
+void serialInit(void)		//9600bps@12.000MHz
+{
+	PCON &= 0x7F;		//Baudrate no doubled
+	SCON = 0x50;		//8bit and variable baudrate
+	AUXR |= 0x04;		//BRT's clock is Fosc (1T)
+	BRT = 0xD9;		//Set BRT's reload value
+	AUXR |= 0x01;		//Use BRT as baudrate generator
+	AUXR |= 0x10;		//BRT running
+}
+void serialSendHex(unsigned char Data){
+	SBUF = Data;
+	while(!TI);
+	TI = 0;
+}
+void Delay500ms()		//@11.0592MHz
+{
+	unsigned char i, j, k;
+
+	_nop_();
+	_nop_();
+	i = 22;
+	j = 3;
+	k = 227;
+	do
+	{
+		do
+		{
+			while (--k);
+		} while (--j);
+	} while (--i);
+}
